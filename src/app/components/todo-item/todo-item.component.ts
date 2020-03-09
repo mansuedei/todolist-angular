92d5b9fde3e2b5d498007379e7442add3c629335
Yana Stepanova / mansuedei <mansuedei@yandex.ru>
2020-03-09 15:44:47 +0300

    added features: adding todo, deleting todo; changed data source to jsonplaceholder

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { TodoService } from "../../services/todo.service";
 import { Todo } from "src/app/models/Todo";
 
 @Component({
@@ -8,7 +9,8 @@ import { Todo } from "src/app/models/Todo";
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
-  constructor() {}
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {}
 
@@ -24,10 +26,16 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
-    console.log("toggle");
+    // Toggle in UI
+    todo.completed = !todo.completed;
+
+    // Toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo);
+    });
   }
 
   onDelete(todo) {
-    console.log("delete");
+    this.deleteTodo.emit(todo);
   }
 }
